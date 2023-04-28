2f814a81943ca487886b08dc656bd1eafe1f0881
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-04-28 21:11:41 +0200

    Created Repo

---
+++ jobsportal/settings/_development_settings.py
@@ -0,0 +1,8 @@
+# Configuration-folder/settings/_development_settings.py
+# Add security related items in this file such as passwords and keys
+
+# Add this file in '.gitignore'
+
+# import everything from settings.py i.e. import os etc.
+from .settings import *
+
