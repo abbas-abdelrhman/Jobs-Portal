8e0a753c75ece4556ca2a6e2710feb0e1a163c1d
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-04-29 00:11:32 +0200

    created .gitignore file

--- jobsportal/settings/_development_settings.py
+++
@@ -1,8 +0,0 @@
-# Configuration-folder/settings/_development_settings.py
-# Add security related items in this file such as passwords and keys
-
-# Add this file in '.gitignore'
-
-# import everything from settings.py i.e. import os etc.
-from .settings import *
-
