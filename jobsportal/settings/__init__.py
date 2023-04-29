ae1d635bf7d0ef6a1b6a56379f93aa3f75b4671a
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-04-29 20:12:04 +0200

    configured psql database for production

--- jobsportal/settings/__init__.py
+++ jobsportal/settings/__init__.py
@@ -1,23 +1,5 @@
 # Configuration-folder/settings/__init__.py
 
-# Do not forget to add '_development_settings.py' in .gitignore
-
-
-# Since '_development_settings' is in gitigonre, hence _production_settings
-# will be selected for online-server
-
-# select 'development' or 'production' server
-# from ._development_settings import *
-
-
-# from .settings import DEBUG
-#
-# if DEBUG:
-#     from ._development_settings import *
-# else:
-#     from ._production_settings import *
-#
-#
 
 try:
     from ._development_settings import *
--- jobsportal/settings/_production_settings.py
+++ jobsportal/settings/_production_settings.py
@@ -1,39 +1,57 @@
 # Configuration-folder/settings/_production_settings.py
 # Add security related items in this file such as passwords and keys
 
-# import everything from settings.py i.e. import os etc.
 from .settings import *
 import os
+import environ
+
+env = environ.Env()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
 # since DEBUG is False, therefore it is compulsory to add ALLOWED_HOSTS
-ALLOWED_HOSTS = ['*']  # add correct hostname i.e. website name
+ALLOWED_HOSTS = ['0.0.0.0']  # add correct hostname i.e. website name
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Create new secret key (just add few more letters in original key)
+SECRET_KEY = env("SECRET_KEY")
 
-SECRET_KEY = 'Wa@pJ*S0pmfqKuMChbfM2qwFEx8C8284P@F8Y0HiKhWfBFX%i9'
 
 # Database : Add production database details here
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 # Email
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# EMAIL_HOST_USER='gbttrial@gmail.com'
-# EMAIL_HOST_PASSWORD='znhhrdibgbdnjjlm'
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+# production security setting
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+ADMINS = [('abbas', "gbttrial@gmail.com")]
+MANAGERS = [('abbas', "gbttrial@gmail.com")]
+
+# MIDDLEWARE += ['django.middleware.common.BrokenLinkEmailsMiddleware']
+
 # #crispy form settings
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+# CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # #django-registration-redux settings
 # ACCOUNT_ACTIVATION_DAYS=7
@@ -58,28 +76,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # # )
 
 
-# production setting
-
-SECURE_SSL_REDIRECT = True
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 3600
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
-# logging configuration
-
-# LOGGING = {
-#     "version": 1,  # the dictConfig format version
-#     "disable_existing_loggers": False,  # retain the default loggers
-#     "handlers": {
-#         "file": {
-#             "class": "logging.FileHandler",
-#             "filename": "general.log",
-#         },
-#     },}
-
-ADMINS = [('abbas', "gbttrial@gmail.com")]
-MANAGERS = [('abbas', "gbttrial@gmail.com")]
-
-# MIDDLEWARE += ['django.middleware.common.BrokenLinkEmailsMiddleware']
+# EMAIL_HOST_USER='gbttrial@gmail.com'
+# EMAIL_HOST_PASSWORD='znhhrdibgbdnjjlm'
