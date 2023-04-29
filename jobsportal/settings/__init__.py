6b3226d26f18fa6cf4e04e32ec9d6ce7a7e393f2
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-04-29 18:33:51 +0200

    make it better

--- jobsportal/settings/__init__.py
+++ jobsportal/settings/__init__.py
@@ -7,7 +7,19 @@
 # will be selected for online-server
 
 # select 'development' or 'production' server
-try:
-    from ._development_settings import *
-except:
-    from ._production_settings import *
+from ._development_settings import *
+
+
+# from .settings import DEBUG
+#
+# if DEBUG:
+#     from ._development_settings import *
+# else:
+#     from ._production_settings import *
+#
+#
+
+# try:
+#     from ._development_settings import *
+# except:
+#     from ._production_settings import *
