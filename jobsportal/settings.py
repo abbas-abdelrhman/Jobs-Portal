2cf13f2a32cc8e0232d04103856c9f077a9c8cbc
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-05-01 17:15:40 +0200

    changed db config

--- jobsportal/settings.py
+++ jobsportal/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'jobsportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jobs_portal',
         'USER': 'abbas',
         'PASSWORD': 'aoe!123',
