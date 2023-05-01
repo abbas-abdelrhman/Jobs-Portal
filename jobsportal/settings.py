5c2e168e356688d94588f8bdba854ad5b5c55390
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-05-01 16:50:40 +0200

    added psql db

--- jobsportal/settings.py
+++ jobsportal/settings.py
@@ -93,21 +93,14 @@ WSGI_APPLICATION = 'jobsportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'jobs_portal',
-#         'USER': 'abbas',
-#         'PASSWORD': 'aoe!123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jobs_portal',
+        'USER': 'abbas',
+        'PASSWORD': 'aoe!123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
