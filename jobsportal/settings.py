7b38ab72f89313d3b7d6dcd9ce2d7d095a88ac33
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-05-07 08:11:17 +0200

    added allowed hosts

--- jobsportal/settings.py
+++ jobsportal/settings.py
@@ -12,8 +12,6 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []  # add correct hostname i.e. website name
+ALLOWED_HOSTS = ['*']  # add correct hostname i.e. website name
 
 SECRET_KEY = 'MEHER1@f#^)9*6dqd5g1jfnnnasww)f2wg-8fkidxdsafan-99@as'
 
@@ -170,6 +168,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-
 
 django_heroku.settings(locals())
