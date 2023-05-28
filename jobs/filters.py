1ededf84eb4911ac016207b6fbbeef73b63a7706
abbas-abdelrhman <a.elrhmanabbas@gmail.com>
2023-05-28 19:13:38 +0200

    Edited filter fields

--- jobs/filters.py
+++ jobs/filters.py
@@ -3,10 +3,9 @@ from .models import Job
 
 
 class JobFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
+    job_title = django_filters.CharFilter(lookup_expr='icontains')
     description = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Job
-        fields = '__all__'
-        exclude = ['published_at', 'image', 'salary', 'Vacancy', 'slug']
+        fields = ['job_title', 'job_type', 'category', 'description']
