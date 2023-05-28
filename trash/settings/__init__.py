f06bbdce608abfec910f2c068db125c290257fda
Abdulrahman Abbas <54155369+abbas-abdelrhman@users.noreply.github.com>
2023-05-29 00:12:37 +0200

    Delete trash/settings directory

--- trash/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-# Configuration-folder/settings/__init__.py
-
-# # try:
-# #     from ._development_settings import *
-# # except:
-# #     from ._production_settings import *
-#
-# from ._production_settings import *
